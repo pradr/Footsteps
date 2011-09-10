a3c5091823725da2f67199a5b08ef2dc538068a9
Rambo <praad.kumar@gmail.com>
2011-09-10 01:39:43 -0400

    One

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Footsteps::Application.config.secret_token = '4a400f179cecb6c12cd7756448027a470069512f7f79e2865f4995fe1eb7c2ff42942139c16b13d683d370dbd3c68748971c6b8342d10db4f6fd365e4e5ec0cc'
